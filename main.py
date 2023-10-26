20b5997a9e1351df9ceaf7599fa6d2dac26c8150
gniraj1432 <27806591-gniraj1432@users.noreply.replit.com>
2023-10-26 16:45:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+# Project 1
+# Making simple calculator using python 
+# print("Calculator")
+# a = int(input("Enter the first number: "))
+# b = int(input("Enter the second number: "))
+# print("1.Addition", a + b)
+# print("2.Subtraction", a - b)
+# print("3.Multiplication", a * b)
+# print("4.Division", a / b)
+# print("5.Modulus", a % b)
+# print("6.Power", a**b)
+# print("7.Floor Division", a // b)
+# print("8.Square Root", a**(1 / 2), b**(1 / 2))
+
+
+# Project 2
+# To find the length of a string
+# print("Length of a string")
+# str = input("Enter the string: ")
+# print("Length of the string is: ", len(str))
+
+
+# Project 3
+# To find the length of a string without using len()
+# print("Length of a string without using len()")
+# str = input("Enter the string: ")
+# count = 0
+# for _i in str:
+#     count += 1
+# print("Length of the string is: ", count)
+
+
+# Project 4
